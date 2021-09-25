ca24e2db655d8065b5889acd8eb585d85ac03705
Nicky Setyawan D <71703749+sameh4d4@users.noreply.github.com>
2021-09-25 16:19:07 +0700

    Create fragments

--- app/src/main/java/com/example/advnative_project_uts_160418066/view/MainActivity.kt
+++ app/src/main/java/com/example/advnative_project_uts_160418066/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.advnative_project_uts_160418066
+package com.example.advnative_project_uts_160418066.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.advnative_project_uts_160418066.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
