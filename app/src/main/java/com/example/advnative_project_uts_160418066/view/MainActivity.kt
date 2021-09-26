deeeecb30038b52e58e6e2b1487e6da85c43ce86
Nicky Setyawan D <71703749+sameh4d4@users.noreply.github.com>
2021-09-26 15:10:37 +0700

    Nav done tinggal trigger di fragment

--- app/src/main/java/com/example/advnative_project_uts_160418066/view/MainActivity.kt
+++ app/src/main/java/com/example/advnative_project_uts_160418066/view/MainActivity.kt
@@ -3,21 +3,30 @@ package com.example.advnative_project_uts_160418066.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.advnative_project_uts_160418066.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
         navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
+
+        bottomNav.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController,null)||super.onSupportNavigateUp()
+        return NavigationUI.navigateUp(navController,drawerLayout)||super.onSupportNavigateUp()
     }
 }
