ad27777f09bf0fc21f521636381800498d19520e
Nicky Setyawan D <71703749+sameh4d4@users.noreply.github.com>
2021-09-26 15:24:42 +0700

    model done

---
+++ app/src/main/java/com/example/advnative_project_uts_160418066/model/Model.kt
@@ -0,0 +1,24 @@
+package com.example.advnative_project_uts_160418066.model
+
+data class User(
+    val mrn:String?,
+    val name:String?,
+    val tglLahir:String?,
+    val noHp:String?,
+    val email:String?,
+    val kelamin:String?
+)
+
+data class Dokter(
+    val nama:String?,
+    val spesialisasi:String?,
+    val noHp:String?,
+    val email:String?,
+    val alamat:String?,
+    val gambar: String?,
+)
+
+data class Fasilitas(
+    val nama:String?,
+    val gambar:String?,
+)
