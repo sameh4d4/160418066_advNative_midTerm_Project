2efd7499a6166ac388e1d6ed5dd02cfb988f7a77
Nicky Setyawan D <71703749+sameh4d4@users.noreply.github.com>
2021-09-28 17:54:15 +0700

    event done

--- app/src/main/java/com/example/advnative_project_uts_160418066/view/EventFragment.kt
+++ app/src/main/java/com/example/advnative_project_uts_160418066/view/EventFragment.kt
@@ -1,11 +1,14 @@
 package com.example.advnative_project_uts_160418066.view
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.advnative_project_uts_160418066.R
+import kotlinx.android.synthetic.main.fragment_event.*
 
 class EventFragment : Fragment() {
     override fun onCreateView(
@@ -18,5 +21,21 @@ class EventFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        btnChekKodeEvent.setOnClickListener {
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Cek Kode")
+            if(txtInputKode.text.toString()=="001"){
+                builder.setMessage("Kode benar")
+            }
+            else{
+                builder.setMessage("Kode salah")
+            }
+            builder.setPositiveButton(android.R.string.ok) { dialog, which ->
+                Toast.makeText(context,
+                    android.R.string.yes, Toast.LENGTH_SHORT).show()
+            }
+            builder.show()
+        }
+
     }
 }
